455d9e131fa05296b6dfe5387042ac5b55bc085c
MicheleGennari <michele.gennari.lavoro@gmail.com>
2024-10-08 13:42:26 +0200

    added relay config file

---
+++ relay.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  src: "./",
+  artifactDirectory: "./__generated__",
+  language: "typescript",
+  eagerEsModules: false,
+  schema: "./schema.graphql",
+  excludes: ["**/node_modules/**", "**/__mocks__/**", "**/__generated/**"],
+};
